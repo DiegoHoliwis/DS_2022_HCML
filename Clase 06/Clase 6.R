12731316f4bd183a82e95f742acde62e711c7b87
Diego Muñoz <dimunozu@gmail.com>
2022-06-01 18:57:43 -0400

    .

--- Clase 06/Clase 6.R
+++ Clase 06/Clase 6.R
@@ -1,1 +1,32 @@
-library(tidyverse)
+# Clase 6 - estructuras de control -----
+
+## operadores lógicos ----
+
+2 < 3
+3 < 2
+
+2 <= log(4)
+3 <= log(100)
+
+paises <- c('chile','perú','brasil')
+
+!'canada' %in% paises
+
+PIB_PAISES <- tibble(pais = c('chile','canada','EEUU'),
+       PIB = c(33,23,50))
+
+PIB_PAISES %>% 
+  filter(!pais %in% paises)
+
+
+PIB_PAISES %>% 
+  filter(!pais %in% paises & PIB > 40)
+
+"Gato" == "gato"
+
+str_to_lower("Gato") == str_to_lower("gato")
+
+letters
+LETTERS
+
+str_to_lower(c("a","B","c")) %in% letters
