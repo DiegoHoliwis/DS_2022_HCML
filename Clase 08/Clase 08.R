93936a7f776f6d01305976eb36001c8ed046eae4
Camila Paredes <ceparedes@uc.cl>
2022-06-15 19:06:51 -0400

    Cargar la base de datos anime

--- Clase 08/Clase 08.R
+++ Clase 08/Clase 08.R
@@ -1,5 +1,48 @@
 # Clase 08 ------
-# Gráficos con ggplot2 ----
+
+# firacode
+library(tidyverse)
+library(datos)
+library(patchwork)
+library(ggcorrplot)
+
+# Repaso
+getwd() # Estoy parado en la ruta de mi proyecto
+setwd('Clase 08') # Cambio mi ruta a la carpeta clase 08
+
+# Lectura desde Internet
+anime <- read_csv2('https://raw.githubusercontent.com/DiegoHoliwis/DS_2022_HCML/main/Clase%2008/anime.csv')
+
+# read_csv() # Asume que tiene separación por ,
+# read_csv2()  # Asume que tiene separación por ;
+
+anime <- read_csv2('anime.csv')
+
+# read_csv(file,  locale = locale(encoding = "Latin1"))
+# data.table::fread(ecoding = 'Latin1')
+
+anime %>% 
+  glimpse()
+
+anime <- anime %>% 
+  filter(episodes != 'Unknown')
+
+
+anime <- anime %>% 
+  mutate(episodes = as.numeric(episodes))
+
+anime %>% 
+  glimpse()
+
+
+anime2 <- anime %>% 
+  mutate(genre = str_split(genre, pattern = ',')) %>% View()
+  unnest(genre) %>% 
+  mutate(genre = str_trim(genre)) # str_trim elimina los espacios al principio y al final
+
+
+
+'Romantico, acción' %>% str_split(',')
 
 
 
