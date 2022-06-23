206d7fe67b8442114db44c872ed46b7686911951
Diego Muñoz <dimunozu@gmail.com>
2022-06-22 20:51:28 -0400

    .

--- Clase 09/Script clase 9.R
+++ Clase 09/Script clase 9.R
@@ -10,7 +10,8 @@ showtext_auto()    # Activa los nuevos fonds
 library(ggcorrplot)
 library(gganimate)
 # library(plotly)
-
+# install.packages('GGally')
+library(GGally)
 
 windowsFonts()
 
@@ -107,3 +108,113 @@ ggplot(data=paises,
                      limits = c(30,60)) + 
   scale_y_continuous(n.breaks = 15,
                      limit = c(0,10000))
+
+
+# Clase numero 9 -----
+
+base_chile <- paises %>% 
+  filter(pais=="Chile")
+base_vecinos <- paises %>% 
+  filter(pais %in% c("Chile","Perú","Bolivia","Argentina"))
+america07 <- paises %>% 
+  filter(anio==2007, continente == "Américas")
+
+# Funcionalidades de resumen
+# ..count..
+# ..prop..
+# ..density..
+
+anime %>% 
+  filter(!is.na(type)) %>% 
+  ggplot(aes(x = type)) +
+  geom_bar(aes(y = ..prop.., group = 1)) +
+  # geom_bar(aes(y = ..count../sum(..count..))) +
+  scale_y_continuous(labels = scales::percent)
+
+
+
+anime %>% 
+  filter(!is.na(type)) %>% 
+  group_by(type) %>% 
+  summarise(count = n()) %>% 
+  mutate(proporción = count/sum(count)) %>% 
+  ggplot(aes(x = type, y = proporción)) +
+  geom_bar(stat = 'identity')
+
+
+# Diferencias entre usar group vs no usarl
+
+# Usando group
+mtautos %>% 
+  ggplot(aes(x = factor(cilindros), group = factor(cambios))) +
+  geom_bar(aes(y = ..prop.., fill = factor(..x..))) +
+  facet_grid(~cambios)
+           
+# Sin usar group
+mtautos %>% 
+  ggplot(aes(x = factor(cilindros))) +
+  geom_bar(aes(y = ..count../sum(..count..),fill = factor(..x..))) +
+  facet_grid(~cambios)
+
+
+mtautos %>% 
+  group_by(cilindros,cambios) %>% 
+  summarise(cantidad = n(), .groups = 'drop') %>% 
+  mutate(proporcion = cantidad/sum(cantidad)) %>% 
+  ggplot(aes(x = cilindros, y = proporcion)) +
+  geom_bar(stat = 'identity') +
+  facet_grid(~cambios)
+
+
+# Gráfico de torta
+ 
+df_cilindros = with(mtautos, table(cilindros)) %>% 
+  data.frame()
+
+#equivalente: mtautos %>% pull(cilindros) %>% table() %>% data.frame()
+
+df_cilindros %>% 
+  mutate(porcentaje = 100*Freq/sum(Freq)) %>% 
+  ggplot(aes(x = Freq, y = '', fill = cilindros)) +
+  geom_bar(stat = 'identity', width = 1) +
+  coord_polar('x') +
+  theme_void() +
+  geom_text(aes(label = porcentaje),
+            size = 10,
+            position = position_stack(vjust=0.5))
+
+
+df_cilindros %>% 
+  mutate(porcentaje = 100*Freq/sum(Freq)) %>% 
+  ggplot(aes(y = porcentaje, x = '', fill = cilindros)) +
+  geom_bar(stat = 'identity', width = 1) +
+  geom_text(aes(label = paste0(round(porcentaje,1),'%')),
+            size = 10,
+            position = position_stack(vjust=0.5)) +
+  coord_polar('y') +
+  theme_void()
+
+# geom_abline
+
+ggplot(data = mtautos, aes(x=millas, y=velocidad))+
+  geom_point(size = 3, shape = 21, fill = "turquoise") +
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  scale_y_continuous(limits = c(0,30)) +
+  scale_x_continuous(limits = c(0,30))
+
+
+ggplot(data = mtautos, aes(x=millas, y=velocidad))+
+  geom_point(size = 3, shape = 21, fill = "turquoise") +
+  stat_smooth(method = "lm", se = F)
+
+# GGpairs
+
+
+
+
+
+
+
+
+
+
