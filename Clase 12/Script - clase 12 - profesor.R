f3ded9462ff5913ca7ddf9e906344e6f4c1bcaa8
Diego Muñoz <dimunozu@gmail.com>
2022-07-20 19:30:52 -0400

    .

--- Clase 12/Script - clase 12 - profesor.R
+++ Clase 12/Script - clase 12 - profesor.R
@@ -43,4 +43,54 @@ summary(mod1)
 # H0: Beta_edad == 0 (La variable NO es significativa)
 # H1: Beta_edad != 0 (La variable ES significativa)
 
+# Separación en train and test
+
+
+# Paso 1: Separar la base de datos en train and test
+# Paso 2: Modelar usando la base de train
+# Paso 2.1: Ajustar hiperparametros (no se aplica en regresión logísitca)
+# Paso 3: Validar usando la base de test
+
+set.seed(123)
+# Selecciona el 70 de los datos
+train <- df %>% sample_frac(0.7) 
+
+# Selecciona los datos del df que no esten en la base train
+test  <- df %>% filter(!row.names(.) %in% row.names(train)) 
+
+
+mod1 <- glm(formula = salario ~ edad + estado_civil + raza + sexo + nativo + horas_semanal,
+            data    = train,
+            family  = binomial(link = 'logit'))
+
+summary(mod1)
+
+# Predicción
+
+predict(mod1,
+        newdata = test,
+        type = 'response')
+
+
+train$salario %>% mean()
+
+# ¿Qué ocurre cuando existe desbalance en la base datos?
+# Los modelos de regresión logística están basados en la idea de que existen
+# la misma cantidad de observaciones de las dos clases (misma cantidad de 0 y 1)
+# Cuando esto no ocurre, en algunos casos el modelo podría llegar aprender muy
+# bien de una clase y muy mal de la otra (normalmente mal de la que nos interesa
+# Predecir). Para solucionar estas problemáticas existen varias técnicas, por ejemplo:
+
+# Opción 1: Eliminar usuarios de la clase con mayor representatividad
+# Opción 2: Incluir varias veces a los usuarios con la clase menor
+# Opción 3: Utilizar las dos técnicas anteriores al mismo tiempo
+# Opción 4: Utilizar regularización L1 y L2
+# Opción 5: Utilizar un modelo que no se vea afectado por desbalance :D
+
+
+
+
+
+
+
 
